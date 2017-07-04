4af8d858495ce9c71f0238852df1beefe64fbfc0
Administrator <beibei>
2017-07-04 11:34:32 +0800

    配置数据库，创建省市县的实体类

---
+++ app/src/main/java/com/example/clweather/db/Province.java
@@ -0,0 +1,41 @@
+package com.example.clweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * 创建时间：2017/7/4.
+ * <p>
+ * 类备注：省的实体类
+ */
+
+public class Province extends DataSupport {
+//    LitePal中的每一个类都必须继承DataSupport类
+
+    private int id;
+    private String provinceName;//省的名字
+    private int provinceCode;//省的代号
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
